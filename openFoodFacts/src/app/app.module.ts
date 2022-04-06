703688fcaf9ff0d0257d80971be28ed41303551c
Luca1204 <lombardi.luca.remo@iisgalvanimi.edu.it>
2022-04-06 11:53:49 +0000

    punto2

--- openFoodFacts/src/app/app.module.ts
+++ openFoodFacts/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { CaratteristicheComponent } from './caratteristiche/caratteristiche.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    CaratteristicheComponent
   ],
   imports: [
     BrowserModule,
---
+++ openFoodFacts/src/app/caratteristiche/caratteristiche.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CaratteristicheComponent } from './caratteristiche.component';
+
+describe('CaratteristicheComponent', () => {
+  let component: CaratteristicheComponent;
+  let fixture: ComponentFixture<CaratteristicheComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CaratteristicheComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CaratteristicheComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
